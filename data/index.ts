e33bbc9910c36fddba6345fe9d0d74b4e3fd6b63
unknown <glennntoutoume8@.com>
2025-06-05 11:32:19 +0200

    Ajout des medias

---
+++ data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+  { name: "À propos", link: "#about" },
+  { name: "Projets", link: "#projects" },
+  { name: "Témoignages", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Je priorise la collaboration avec le client, favorisant une communication ouverte",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Je suis très flexible avec les communications跨fuseaux horaires",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Ma stack technique",
+    description: "J'essaie constamment de m'améliorer",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Passionné de technologie avec un amour pour le développement.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Actuellement en train de créer une bibliothèque d'animation JS",
+    description: "Le Scoop Interne",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Voulez-vous démarrer un projet ensemble ?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Planètes du Système Solaire en 3D à Explorer",
+    des: "Explorez les merveilles de notre système solaire avec cette simulation 3D captivante des planètes utilisant Three.js.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://github.com/adrianhajdin?tab=repositories",
+  },
+  {
+    id: 2,
+    title: "Yoom - Application de Visioconférence",
+    des: "Simplifiez votre expérience de visioconférence avec Yoom. Connectez-vous facilement avec vos collègues et amis.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://github.com/adrianhajdin/zoom-clone",
+  },
+  {
+    id: 3,
+    title: "SaaS d'Image IA - Application type Canva",
+    des: "Une VRAIE application Software-as-a-Service avec des fonctionnalités IA et un système de paiements et de crédits utilisant la dernière stack technologique.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://github.com/adrianhajdin/ai_saas_app",
+  },
+  {
+    id: 4,
+    title: "Site Web Animé Apple iPhone 3D",
+    des: "Recréation du site web de l'Apple iPhone 15 Pro, combinant des animations GSAP et des effets 3D Three.js.",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://github.com/adrianhajdin/iphone",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborer avec Adrian a été un plaisir absolu. Son professionnalisme, sa ponctualité et son dévouement à fournir des résultats exceptionnels étaient évidents tout au long de notre projet. L'enthousiasme d'Adrian pour chaque facette du développement se démarque vraiment. Si vous cherchez à améliorer votre site web et à rehausser votre marque, Adrian est le partenaire idéal.",
+    name: "Michel Dupont",
+    title: "Directeur de AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborer avec Adrian a été un plaisir absolu. Son professionnalisme, sa ponctualité et son dévouement à fournir des résultats exceptionnels étaient évidents tout au long de notre projet. L'enthousiasme d'Adrian pour chaque facette du développement se démarque vraiment. Si vous cherchez à améliorer votre site web et à rehausser votre marque, Adrian est le partenaire idéal.",
+    name: "Michel Dupont",
+    title: "Directeur de AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborer avec Adrian a été un plaisir absolu. Son professionnalisme, sa ponctualité et son dévouement à fournir des résultats exceptionnels étaient évidents tout au long de notre projet. L'enthousiasme d'Adrian pour chaque facette du développement se démarque vraiment. Si vous cherchez à améliorer votre site web et à rehausser votre marque, Adrian est le partenaire idéal.",
+    name: "Michel Dupont",
+    title: "Directeur de AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborer avec Adrian a été un plaisir absolu. Son professionnalisme, sa ponctualité et son dévouement à fournir des résultats exceptionnels étaient évidents tout au long de notre projet. L'enthousiasme d'Adrian pour chaque facette du développement se démarque vraiment. Si vous cherchez à améliorer votre site web et à rehausser votre marque, Adrian est le partenaire idéal.",
+    name: "Michel Dupont",
+    title: "Directeur de AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborer avec Adrian a été un plaisir absolu. Son professionnalisme, sa ponctualité et son dévouement à fournir des résultats exceptionnels étaient évidents tout au long de notre projet. L'enthousiasme d'Adrian pour chaque facette du développement se démarque vraiment. Si vous cherchez à améliorer votre site web et à rehausser votre marque, Adrian est le partenaire idéal.",
+    name: "Michel Dupont",
+    title: "Directeur de AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Stagiaire Ingénieur Frontend",
+    desc: "A participé au développement d'une plateforme web en utilisant React.js, améliorant l'interactivité.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Développeur d'Applications Mobiles - JSM Tech",
+    desc: "Conception et développement d'une application mobile pour les plateformes iOS et Android en utilisant React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Projet de Développement d'Application en Freelance",
+    desc: "Direction du développement d'une application mobile pour un client, du concept initial au déploiement sur les stores d'applications.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Développeur Frontend Principal",
+    desc: "Développement et maintenance de fonctionnalités orientées utilisateur en utilisant des technologies frontend modernes.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
