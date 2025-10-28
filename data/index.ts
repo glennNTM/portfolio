4c8c16bfdc04b8c5145a6da8fba68a0f54fdf613
glennNTM <glenn.ntoutoume.dev@gmail.com>
2025-10-28 22:34:06 +0100

    Upadate de la section des projets

--- data/index.ts
+++ data/index.ts
@@ -12,61 +12,62 @@ export const projects = [
     id: 1,
     title: "Site de Ecole241 Communities",
     des: "Plateforme web LMS pour le programme communities d'Ecole241.",
-    imgList: ["/Macbook-Air-241-communities.vercel.app (1).png", "/Macbook-Air-241-communities.vercel.app.png"], // Slider prêt pour plusieurs images
+    image: "/241communities.png",
+    status: "Déployé",
     keyFunction: [
       "Présentation des programmes et formations",
       "Formulaire d'inscription interactif",
       "Design responsive et moderne",
       "Informations sur les événements et actualités"
     ],
-    iconLists: ["/ts.svg", "/postgresql-logo-svgrepo-com.svg", "/next.svg", "/tail.svg"],
-    link: "https://241-communities.vercel.app/",
-    linkType: "deploy",
+    technologies: ["/ts.svg", "/postgresql-logo-svgrepo-com.svg", "/next.svg", "/tail.svg"],
+    deployLink: "https://241-communities.vercel.app/",
+    githubLink: "https://github.com/glennNTM/241-communities",
   },
-{
+  {
     id: 2,
     title: "Doctely",
-    des: "Plateforme web de teleconsultation medicale.",
-    imgList: ["/Macbook-Air-localhost.png", "/doctelyscreenshot2.png"],
+    des: "Plateforme web de téléconsultation médicale.",
+    image: "/doctely.png",
+    status: "Déployé",
     keyFunction: [
-      "Teleconsultation medicale avec des medecins",
-      "Ordonnaces numériques",
+      "Téléconsultation médicale avec des médecins",
+      "Ordonnances numériques",
       "Gestion des patients et des médecins",
-      "Securité des données médicales"
+      "Sécurité des données médicales"
     ],
-    iconLists: ["/postgresql-logo-svgrepo-com.svg", "/express-svgrepo-com.svg", "/re.svg", "/nodejs-svgrepo-com.svg"], 
-    link: "https://doctely.netlify.app/",
-    linkType: "deploy",
+    technologies: ["/postgresql-logo-svgrepo-com.svg", "/express-svgrepo-com.svg", "/re.svg", "/nodejs-svgrepo-com.svg"],
+    deployLink: "https://doctely.netlify.app/",
+    githubLink: "https://github.com/glennNTM/doctely",
   },
   {
     id: 3,
     title: "Tasky",
     des: "Application de gestion de tâches en ligne.",
-    imgList: ["/Macbook-Air-localhost (1).png"],
+    image: "/Macbook-Air-localhost (1).png",
+    status: "En cours de développement",
     keyFunction: [
       "Création, lecture, mise à jour, suppression (CRUD) de tâches",
       "Système d'authentification des utilisateurs avec JWT",
       "Interface utilisateur responsive",
-      "Securité contre les attaques XSS et CSRF"
+      "Sécurité contre les attaques XSS et CSRF"
     ],
-    iconLists: ["mongodb-svgrepo-com.svg","express-svgrepo-com.svg", "/re.svg", "/nodejs-svgrepo-com.svg"],
-    link: "https://github.com/glennNTM/Tasky",
-    linkType: "github", 
+    technologies: ["/mongodb-svgrepo-com.svg", "/express-svgrepo-com.svg", "/re.svg", "/nodejs-svgrepo-com.svg"],
+    githubLink: "https://github.com/glennNTM/tasky",
   },
-
   {
     id: 4,
     title: "Promptopia",
     des: "Plateforme pour partager et récupérer des prompts pertinents.",
-    imgList: ["/promptopia.png"],
+    image: "/promptopia.png",
+    status: "En cours de développement",
     keyFunction: [
       "Partage et découverte de prompts pour IA",
       "Recherche et filtrage de prompts par tag",
       "Authentification OAuth 2.0 via Google"
     ],
-    iconLists: ["/javascript-svgrepo-com (1).svg", "/mongodb-svgrepo-com.svg", "/next.svg", "/tail.svg"],
-    link: "https://github.com/glennNTM/promptopia.git",
-    linkType: "github",
+    technologies: ["/javascript-svgrepo-com (1).svg", "/mongodb-svgrepo-com.svg", "/next.svg", "/tail.svg"],
+    githubLink: "https://github.com/glennNTM/promptopia",
   },
 ];
 
